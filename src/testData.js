9a1c349db14095143f14d0fbdc60c4fb9485cd0c
elvstejd <elvistejeda0910@gmail.com>
2021-08-29 10:18:32 -0400

    Modify test data schema

--- src/testData.js
+++ src/testData.js
@@ -4,48 +4,65 @@ const listings = [
         precio: '10,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.470133,
-        long: -69.937438
+        location: {
+            lat: 18.470133,
+            long: -69.937438
+        }
     },
     {
         id: 'something2',
         precio: '50,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.460852,
-        long: -69.943622
+        location: {
+            lat: 18.460852,
+            long: -69.943622
+        }
+
     },
     {
         id: 'something3',
         precio: '40,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.461015,
-        long: -69.932972
+        location: {
+            lat: 18.461015,
+            long: -69.932972
+        }
+
     },
     {
         id: 'something4',
         precio: '40,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.442615,
-        long: -69.943450
+        location: {
+            lat: 18.442615,
+            long: -69.943450
+        }
+
     },
     {
         id: 'something5',
         precio: '40,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.439846,
-        long: -69.962173
+        location: {
+            lat: 18.439846,
+            long: -69.962173
+        }
+
     },
     {
         id: 'something6',
         precio: '40,000',
         direccion: 'Av. Avenida avenida #4ven1d4, esquina avenida',
         sector: 'El sector',
-        lat: 18.458410,
-        long: -69.913905
+        location: {
+            lat: 18.458410,
+            long: -69.913905
+        }
+
     }
 ];
 
