3654ec8ae7defa3f9f4a307d91dae33330e63fb5
elvstejd <elvistejeda0910@gmail.com>
2021-09-01 14:13:26 -0400

    Create bare dashboard page

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,38 @@
+import { Divider, Heading, Box, UnorderedList, ListItem, Link, Button } from '@chakra-ui/react';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import React from 'react';
+
+function DashboardPage() {
+    const history = useHistory();
+    const { currentUser, logout } = useAuth();
+
+    async function handleLogout() {
+        try {
+            await logout();
+            history.push('/');
+        } catch (err) {
+            console.log('log out error', err);
+        }
+    }
+
+    return (
+        <Box
+            maxW="70rem"
+            margin="0 auto"
+            pt="1rem"
+        >
+            <Heading size="lg">Hola, {currentUser.email}</Heading>
+            <Divider></Divider>
+            <Heading size="md" pt="2rem">Opciones</Heading>
+            <UnorderedList>
+                <ListItem><Link to="/add" as={RouterLink}>Añadir propiedad</Link></ListItem>
+                <ListItem><Button variant="outline" onClick={handleLogout}>Cerrar sesión</Button></ListItem>
+            </UnorderedList>
+
+
+        </Box>
+    )
+}
+
+export default DashboardPage;
